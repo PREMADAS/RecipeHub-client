e02de9cb29b8fbc8132353bb2a8cd65b81514443
PREMADAS <premadas083@gmail.com>
2026-09-04 11:53:53 +0600

    live link

--- next.config.mjs
+++ next.config.mjs
@@ -5,18 +5,24 @@ const nextConfig = {
       {
         protocol: "https",
         hostname: "images.unsplash.com",
-
       },
       {
-        protocol: 'https',
-        hostname: 'i.pinimg.com',
+        protocol: "https",
+        hostname: "i.pinimg.com",
       },
-
       {
         protocol: "https",
         hostname: "www.usnews.com",
       },
     ],
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "https://recipe-hub-server-ten.vercel.app/api/:path*",
+      },
+    ];
   },
 };
 
