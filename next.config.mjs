a38c794735aedfc0f460efed070876269cb6c5da
PREMADAS <premadas083@gmail.com>
2026-09-05 21:39:58 +0600

    recipe client

--- next.config.mjs
+++ next.config.mjs
@@ -2,23 +2,19 @@
 const nextConfig = {
   images: {
     remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "images.unsplash.com",
-
-      },
-      {
-        protocol: 'https',
-        hostname: 'i.pinimg.com',
-      },
-
-      {
-        protocol: "https",
-        hostname: "www.usnews.com",
-      },
+      { protocol: "https", hostname: "images.unsplash.com" },
+      { protocol: 'https', hostname: 'i.pinimg.com' },
+      { protocol: "https", hostname: "www.usnews.com" },
     ],
   },
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: `${process.env.API_URL}/api/:path*`, // server URL, NEXT_PUBLIC_ ছাড়া
+      },
+    ];
+  },
 };
-
 
 export default nextConfig;
